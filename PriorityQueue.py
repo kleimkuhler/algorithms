e90fc4e3e53ed4612419f27b01b98b7512a814e9
Kevin Leimkuhler <me@kleimkuhler.com>
2018-01-22 22:42:09 -0800

    Make PriorityQueue more efficient
    Cut the number of sifts in `make()` by half because that is all that
    is needed to build a max heap.

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -18,21 +18,21 @@ class PriorityQueue:
         "Is the dist of u smaller than the dist of v."
         return self.dist[self.Q[u]] < self.dist[self.Q[v]]
 
-    def _sift(self, i):
-        "Recursively sift a node down the queue."
-        start = i
+    def _sift(self, n, i):
+        "Recursively sift down i if A[i] is smaller than either of its children."
+        largest = i
         left = 2 * i + 1
         right = 2 * i + 2
-        if left < len(self.Q) and self._min(left, start):
-            start = left
-        if right < len(self.Q) and self._min(right, start):
-            start = right
-        if start != i:
-            self.Q[i], self.Q[start] = self.Q[start], self.Q[i]
-            self._sift(start)
+        if left < n and self._min(left, largest):
+            largest = left
+        if right < n and self._min(right, largest):
+            largest = right
+        if largest != i:
+            self.Q[i], self.Q[largest] = self.Q[largest], self.Q[i]
+            self._sift(n, largest)
 
     def _bubble(self, i):
-        "Recursively bubble a node up the queue."
+        "Recursively bubble up i if A[i] is larger than its parent."
         parent = (i - 1) // 2
         if i != 0 and self._min(i, parent):
             self.Q[i], self.Q[parent] = self.Q[parent], self.Q[i]
@@ -41,8 +41,8 @@ class PriorityQueue:
     def make(self):
         "Make a Priority Queue from the set of nodes."
         self.Q.extend(set(self.dist))
-        for i in range(len(self.Q)-1, -1, -1):
-            self._sift(i)
+        for i in range(len(self.Q)//2, -1, -1):
+            self._sift(len(self.Q), i)
 
     def decrease(self, node):
         "Decrease a node in the queue by bubbling up it's new dist value."
@@ -53,7 +53,7 @@ class PriorityQueue:
         u = self.Q.pop(0)
         try:
             self.Q.insert(0, self.Q.pop(-1))
-            self._sift(0)
+            self._sift(len(self.Q), 0)
         except IndexError:
             pass
         return u
