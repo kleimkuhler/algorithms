0888515d7ab27cf1a5f8919c2dc7b0d0e1efaf2c
Kevin Leimkuhler <me@kleimkuhler.com>
2018-01-22 22:30:14 -0800

    Add Bellman Ford solution

--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ def rosalind_index(A):
 
 def rosalind_pretty(s):
     s = str(s)
-    rep = {',': '', '(': '', ')': '', '[': '', ']': ''}
+    rep = {',': '', '(': '', ')': '', '[': '', ']': '', '\'': ''}
     for i, j in rep.items():
         s = s.replace(i, j)
     return s
